fedf7fcf0fcb90b737e6feeed33bfaf147e1ee5f
beccacrosse <109544455+beccacrosse@users.noreply.github.com>
2024-02-17 21:24:09 -0800

    Create Fonts.js

---
+++ Tag/assets/themes/Fonts.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+import Colors from "./Colors";
+const textStyle = StyleSheet.create({
+  question: {
+    fontFamily: "Cutive",
+    fontSize: 20,
+    color: Colors.white,
+  },
+  answer: {
+    fontFamily: "Cutivemono",
+    fontSize: 44,
+    color: Colors.white,
+  },
+  prompt: {
+    fontFamily: "Cutive",
+    fontSize: 20,
+    color: Colors.black,
+  }
+});
+
+export default textStyle;
