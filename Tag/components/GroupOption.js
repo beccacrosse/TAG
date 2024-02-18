c065f815db03b8fa6c7083c171a45c729a182613
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2024-02-18 05:24:44 -0800

    Format

--- Tag/components/GroupOption.js
+++ Tag/components/GroupOption.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
 import Fonts from "../branding/Fonts";
 import Colors from "../branding/Colors";
+import { FontAwesome6 } from "@expo/vector-icons";
 
 const GroupOption = ({ name, onSelect }) => {
   return (
     <TouchableOpacity onPress={() => onSelect(name)} style={styles.container}>
+      <FontAwesome6
+        name="people-group"
+        size={24}
+        color="white"
+        marginHorizontal={10}
+      />
       <Text style={styles.text}>{name}</Text>
     </TouchableOpacity>
   );
@@ -15,13 +22,16 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     marginVertical: 5,
+    marginHorizontal: 5,
+    paddingHorizontal: 5,
+    flexDirection: "row",
   },
   text: {
-    fontSize: 16,
+    fontSize: 20,
     fontWeight: "bold",
     fontFamily: "Cutive",
     color: Colors.white,
-    lineHeight: 20,
+    lineHeight: 30,
   },
 });
 
--- Tag/screens/groups/JoinorCreateScreen.js
+++ Tag/screens/groups/JoinorCreateScreen.js
@@ -1,41 +1,72 @@
-import React, { useState } from 'react';
-import { View, TouchableOpacity, Text, StyleSheet, Dimensions } from 'react-native';
-import TopBar from '../../components/TopBar';
-import GroupSelector from '../../components/GroupSelector';
-import FriendSelector from '../../components/FriendSelector';
+import React, { useState } from "react";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  StyleSheet,
+  Dimensions,
+} from "react-native";
+import TopBar from "../../components/TopBar";
+import GroupSelector from "../../components/GroupSelector";
+import FriendSelector from "../../components/FriendSelector";
 import Colors from "../../branding/Colors";
 
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 const GroupsOrFriends = ({ onSelectionChange, selected }) => {
   return (
     <View style={styles.groupsOrFriendsContainer}>
-      <TouchableOpacity 
-        onPress={() => onSelectionChange('Groups')} 
-        style={[styles.groupsAndFriendsButton, selected === 'Groups' && styles.selectedButton]}>
-        <Text style={[styles.groupsAndFriendsText, selected === 'Groups' && styles.selectedText]}>Groups</Text>
+      <TouchableOpacity
+        onPress={() => onSelectionChange("Groups")}
+        style={[
+          styles.groupsAndFriendsButton,
+          selected === "Groups" && styles.selectedButton,
+        ]}
+      >
+        <Text
+          style={[
+            styles.groupsAndFriendsText,
+            selected === "Groups" && styles.selectedText,
+          ]}
+        >
+          Groups
+        </Text>
       </TouchableOpacity>
       <Text style={styles.groupsAndFriendsText}>/</Text>
-      <TouchableOpacity 
-        onPress={() => onSelectionChange('Friends')} 
-        style={[styles.groupsAndFriendsButton, selected === 'Friends' && styles.selectedButton]}>
-        <Text style={[styles.groupsAndFriendsText, selected === 'Friends' && styles.selectedText]}>Friends</Text>
+      <TouchableOpacity
+        onPress={() => onSelectionChange("Friends")}
+        style={[
+          styles.groupsAndFriendsButton,
+          selected === "Friends" && styles.selectedButton,
+        ]}
+      >
+        <Text
+          style={[
+            styles.groupsAndFriendsText,
+            selected === "Friends" && styles.selectedText,
+          ]}
+        >
+          Friends
+        </Text>
       </TouchableOpacity>
     </View>
   );
 };
 
 const JoinOrCreateScreen = ({ navigation }) => {
-  const [selection, setSelection] = useState('Groups');
+  const [selection, setSelection] = useState("Groups");
 
   return (
     <View style={styles.mainContainer}>
       <TopBar />
       <View style={styles.section}>
-        <GroupsOrFriends onSelectionChange={setSelection} selected={selection} />
+        <GroupsOrFriends
+          onSelectionChange={setSelection}
+          selected={selection}
+        />
       </View>
       <View style={styles.section}>
-        {selection === 'Groups' ? <GroupSelector /> : <FriendSelector />}
+        {selection === "Groups" ? <GroupSelector /> : <FriendSelector />}
       </View>
     </View>
   );
@@ -52,30 +83,33 @@ const styles = StyleSheet.create({
   },
   groupsOrFriendsContainer: {
     paddingTop: height * 0.01,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   },
   groupsAndFriendsButton: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 10,
     marginHorizontal: width * 0.03,
   },
   groupsAndFriendsText: {
-    fontWeight: 'bold',
-    fontSize: 24,
+    fontWeight: "bold",
+    fontSize: 28,
     lineHeight: 30,
-    color: 'white',
+    fontFamily: "CutiveMono",
+    color: "white",
   },
-  selectedButton: { // Assuming you want some styling for the selected button
+  selectedButton: {
+    // Assuming you want some styling for the selected button
     backgroundColor: Colors.lightGrey, // Example color, adjust as needed
   },
   selectedText: {
-    fontWeight: 'bold',
-    fontSize: 28,
-    lineHeight: 34,
-    color: 'white',
+    fontWeight: "bold",
+    fontSize: 34,
+    fontFamily: "Cutive",
+    lineHeight: 40,
+    color: "white",
   },
 });
 
