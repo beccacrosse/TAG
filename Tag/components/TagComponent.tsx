873173eb595553c85717e6c332eb4535f91a4a97
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2024-02-18 00:26:13 -0800

    Update TagComponent.tsx

--- Tag/components/TagComponent.tsx
+++ Tag/components/TagComponent.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, Image, Pressable, StyleSheet } from "react-native";
 import Colors from "../branding/Colors";
 import fonts from "../branding/Fonts";
+import { AntDesign } from "@expo/vector-icons";
 
 const TagComponent = ({ question, numberOfAnswers, images }) => {
+  const [isPressed, setIsPressed] = useState(false);
+  const handlePress = () => {
+    setIsPressed(!isPressed);
+  };
   return (
     <View style={styles.card}>
+      <Pressable style={styles.heart} onPress={handlePress}>
+        <AntDesign
+          name={isPressed ? "heart" : "hearto"}
+          color={isPressed ? "red" : "black"}
+        />
+      </Pressable>
       <Text style={styles.question}>{question}</Text>
       <View style={styles.footer}>
         <Text style={styles.answerCount}>{numberOfAnswers}</Text>
@@ -20,6 +32,11 @@ const TagComponent = ({ question, numberOfAnswers, images }) => {
 };
 
 const styles = StyleSheet.create({
+  heart: {
+    position: "absolute",
+    right: 10,
+    top: 10,
+  },
   card: {
     backgroundColor: "#FFFFFF",
     borderRadius: 8,
@@ -37,7 +54,8 @@ const styles = StyleSheet.create({
     color: "#333",
     fontFamily: fonts.question.fontFamily,
     fontSize: 11,
-    marginVertical: 10,
+    marginTop: 20,
+    textAlign: "center",
     lineHeight: 15,
   },
   footer: {
