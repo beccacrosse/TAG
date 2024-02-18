3ef820cd6f2acf306e81fc52cf32e00d132ef4ae
Julia Rhee <jrhee25@stanford.edu>
2024-02-18 06:46:25 -0800

    home page tweak

--- Tag/screens/signup/LoginScreen0.js
+++ Tag/screens/signup/LoginScreen0.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Pressable } from "react-native";
-import { saveUser, removeAllUsers } from "../../DatabaseManager";
+import { View, Text, StyleSheet, Pressable, Dimensions } from "react-native";
+import { saveUser, removeAllUsers } from "../../DatabaseManager"; // Assuming you have these functions
 import Colors from "../../branding/Colors";
 import fonts from "../../branding/Fonts";
+
+const { width, height } = Dimensions.get("window");
 
 function LoginScreen({ navigation }) {
   const createNewUser = async () => {
@@ -22,13 +24,15 @@ function LoginScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.tagText}>Tag</Text>
-      <Pressable onPress={() => navigation.navigate("Phone6")}>
-        <Text style={styles.text}>sign in with your phone number</Text>
+      <Pressable 
+        onPress={() => navigation.navigate("Phone6")}
+        style={styles.buttonStyle}>
+        <Text style={styles.buttonText}>sign in with your phone number</Text>
       </Pressable>
       <Text style={styles.orText}>OR</Text>
 
-      <Pressable onPress={() => createNewUser()}>
-              <Text style={styles.smallerText}>create a new account</Text>
+      <Pressable onPress={createNewUser} style={styles.buttonStyle2}>
+        <Text style={styles.buttonText2}>create a new account</Text>
       </Pressable>
     </View>
   );
@@ -38,30 +42,49 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: Colors.rootedLight,
     flex: 1,
-    justifyContent: "center",
+    //justifyContent: "center",
     alignItems: "center",
     padding: 20,
   },
-  text: {
-    marginTop: 20,
+  buttonStyle: {
     marginBottom: 5,
-    fontFamily: "Poppins",
-    color: Colors.white,
-    fontSize: 18,
+    backgroundColor: "white",
+    borderRadius: 14, // Adjust this value to get the desired circular shape
+    paddingVertical: 10, // Adjust padding for better fit
+    paddingHorizontal: 18, // Adjust padding for better fit
+    elevation: 2, // Optional, adds a shadow for Android
+    marginTop: height * .48,
   },
-  smallerText: {
+  buttonStyle2: {
+    marginBottom: 5,
+    borderRadius: 20, // Adjust this value to get the desired circular shape
+    paddingVertical: 10, // Adjust padding for better fit
+    paddingHorizontal: 20, // Adjust padding for better fit
+    elevation: 2, // Optional, adds a shadow for Android
+  },
+  buttonText: {
+    fontFamily: "Poppins",
+    color: Colors.rootedLight,
+    fontSize: 18,
+    textAlign: "center", // Ensure text is centered within the button
+  },
+  buttonText2: {
     fontFamily: "Poppins",
     color: Colors.vetiver,
-    fontSize: 16,
+    fontSize: 18,
+    textAlign: "center", // Ensure text is centered within the button
   },
   orText: {
     color: Colors.vetiver,
     marginTop: 10,
-    marginBottom:0
+    marginBottom: 0,
   },
   tagText: {
     ...fonts.loginLogo,
+    color: Colors.white,
     alignContent: "center",
+    fontSize: 100, // Assuming you set this in fonts.loginLogo
+    marginTop: height * 0.13,
   },
 });
 
--- Tag/screens/signup/Name1.js
+++ Tag/screens/signup/Name1.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: Colors.rootedLight,
+    backgroundColor: Colors.violet,
   },
   input: {
     borderWidth: 1,
