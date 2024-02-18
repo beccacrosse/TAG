032105c29f9e692cfcfce0fde4bdeea1d91dd7ae
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2024-02-18 03:39:54 -0800

    back

--- Tag/screens/missing.js
+++ Tag/screens/missing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Colors from "../branding/Colors";
+import FloatingBackButton from "../components/backButton";
 
-const InProgressScreen = () => {
+const InProgressScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
+      <FloatingBackButton navigation={navigation} />
       <Text style={styles.text}>This feature is still in progress.</Text>
       <Text style={styles.text}>Please check back later.</Text>
     </View>
