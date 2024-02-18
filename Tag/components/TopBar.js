31dd7c54982ef61c037bc873e0a4d266841f6d78
Lucy Zimmerman <30590230+lucyzimmo@users.noreply.github.com>
2024-02-17 23:17:00 -0800

    header

--- Tag/components/TopBar.js
+++ Tag/components/TopBar.js
@@ -1,45 +1,53 @@
 import React from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native"; // Ensure you're using React Navigation v5 or later
+import fonts from "../branding/Fonts";
 
 function TopBar() {
   const navigation = useNavigation();
 
   return (
     <View style={styles.container}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("JoinorCreate")}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Groups</Text>
-      </TouchableOpacity>
+      <View style={styles.textContainer}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("JoinorCreate")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Groups</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Home")}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Home</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Home")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Tag</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => navigation.navigate("Profile")}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Profile</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Profile")}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 35,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    height: "auto",
     paddingHorizontal: 10,
     backgroundColor: "#6200EE",
+  },
+  textContainer: {
+    marginTop: 30,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
   title: {
     color: "#FFFFFF",
@@ -48,6 +56,7 @@ const styles = StyleSheet.create({
   buttonText: {
     color: "#FFFFFF",
     fontSize: 16,
+    ...fonts.header,
   },
   button: {
     padding: 10,
--- Tag/firebase.js
+++ Tag/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from '@firebase/firestore';
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBuoMHwFJ2hkcSBdRYzVmKi31FDUmiHl9M",
@@ -10,7 +10,7 @@ const firebaseConfig = {
   storageBucket: "treehacks2024-925f2.appspot.com",
   messagingSenderId: "240657790532",
   appId: "1:240657790532:web:c6e4fcf9f11bc284ec2e11",
-  measurementId: "G-NYBDR5621N"
+  measurementId: "G-NYBDR5621N",
 };
 
 // Initialize Firebase
